9899f12b450e871d9872a2db84ae26fb55311a67
acrm <roman.d.slobodenyuk@ya.ru>
2014-01-27 09:07:43 +0400

    Fixed problem with Toolkit.dll loading

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using LangStat.Core;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,7 +14,9 @@ namespace LanguageStatisticsApp
     {
         [STAThread]
         public static void Main()
-        {            
+        {
+            LoadExtrenalLibraries();
+
             var mainWindow = new MainWindow();
             var viewModel = new MainWindowViewModel(mainWindow);
             var view = new MainWindowView { Model = viewModel };
@@ -21,6 +24,23 @@ namespace LanguageStatisticsApp
             var application = new Application(); 
             application.MainWindow = view;
             application.Run(view);
+        }
+
+        private static void LoadExtrenalLibraries()
+        {
+            var path = "ExtrenalLibraries";
+            if(!Directory.Exists(path)) return;
+            var librariesPathes = Directory.GetFiles(path, "*.dll");
+
+            foreach(var libraryPath in librariesPathes)
+            {
+                using (var stream = new FileStream(libraryPath, FileMode.Open))
+                {
+                    var bytes = new byte[stream.Length];
+                    stream.Read(bytes, 0, bytes.Length);
+                    AppDomain.CurrentDomain.Load(bytes);
+                }
+            }
         }
     }
 }
